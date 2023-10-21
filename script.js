86fbdafea2770a82802abb8397adc5476b190cd6
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-10-21 17:49:32 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,28 @@
+var total = 10 + 5;
+
+console.log(total);
+
+var modulo = 15948431324 / 3;
+var resto = 15948431324 % 3;
+
+console.log(modulo, resto);
+
+var testeNaN = "Isto é 100" / 2;
+console.log(testeNaN);
+
+// Exercicio
+
+var expressao1 = "Teste" / 2;
+var expressao2 = "Teste" - 2;
+console.log(expressao1, expressao2);
+
+var soma = +"200" + 50;
+console.log(soma);
+
+var x = 5;
+console.log(++x);
+
+var numero = +"80" / 2;
+var unidade = "kg";
+var peso = numero + unidade;
+console.log(peso);
